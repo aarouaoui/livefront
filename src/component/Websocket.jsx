47f936276c79eac7f5dd881783742a3065a16901
aarouaoui <79322975+aarouaoui@users.noreply.github.com>
2021-06-30 09:27:49 +0200

    Update App.js

--- src/component/Websocket.jsx
+++ src/component/Websocket.jsx
@@ -1,5 +1,6 @@
 import React , {Component} from "react";
 import List from "./List"
+import axios from 'axios'
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 
 class Websocket extends Component {
@@ -9,14 +10,15 @@ class Websocket extends Component {
         this.state = {
             cards : [],
         }
+        this.initialiseCard();
         this.client = new W3CWebSocket('wss://m1l7vq7ida.execute-api.us-east-2.amazonaws.com/production');
         this.client.onopen = () => {
             console.log('WebSocket Client Connected');
           };
-          this.client.onmessage = (message) => {
-            console.log(message.data);
+          this.client.onmessage = (e) => {
+            var card = JSON.parse(e.data);
             const cards = [ ... this.state.cards]
-            cards.push({content: message.data})
+            cards.push(card)
             this.setState({cards})
           }; 
     }
@@ -31,8 +33,15 @@ class Websocket extends Component {
           this.client.onerror = (error) => {
               console.log(error);
           }
-    }  
+    }
 
+    initialiseCard = () => {
+        const url= "https://4ymem8ew75.execute-api.us-east-2.amazonaws.com/prod/card";
+        axios.get(url).then(response => {
+            console.log(response.data);
+            this.setState({cards : response.data});
+        });
+    } 
     render() {
         return (
             <List elements={this.state.cards} elementClass = "alert alert-danger"/>
